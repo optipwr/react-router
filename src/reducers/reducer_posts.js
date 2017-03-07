d45297d64b9974482b1e6b460613f2fb7359949e
Michael Irby <michael.irbstoomuch@gmail.com>
2017-03-07 10:37:37 -0500

    redux form added

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -2,10 +2,10 @@ import { FETCH_POSTS } from '../actions/index';
 
 const INITIAL_STATE = { all: [], post: null };
 
-export default function (state = INITIAL_STATE, action) {
+export default function(state = INITIAL_STATE, action) {
     switch(action.type) {
     case FETCH_POSTS:
-        return { ...state, all: actions.payload.data };
+        return { ...state, all: action.payload.data };
     default:
         return state;
     }
