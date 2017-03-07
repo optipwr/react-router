620d4a868caa522a85959a8582ebb7bf1d7aa2bf
Michael Irby <michael.irbstoomuch@gmail.com>
2017-03-07 12:50:59 -0500

    form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
     render() {
+        const { fields: { title, categories, content }, handleSubmit } = this.props; // == ES6
+        // const handleSubmit = this.props.handleSubmit; == ES5 version
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
                 <div className="form-group">
                     <label>Title</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" {...title} />
                 </div>
 
                 <div className="form-group">
                     <label>Categories</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" {...categories} />
                 </div>
 
                 <div className="form-group">
                     <label>Content</label>
-                    <textarea className="form-control" />
+                    <textarea className="form-control" {...content} />
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
@@ -27,7 +30,16 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    return errors;
+}
+
+//connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+//reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
 export default reduxForm({
     form: 'PostsNewForm',
     fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
