1af78709b8c6a6070bff1ae6031dedbda2c90bac
Michael Irby <michael.irbstoomuch@gmail.com>
2017-03-08 13:15:38 -0500

    more form stuff

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
     render() {
@@ -9,22 +10,33 @@ class PostsNew extends Component {
         return (
             <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
-                <div className="form-group">
+
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
+                    <div className="text-help">
+                        {title.touched ? title.error : ''}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories} />
+                    <div className="text-help">
+                        {categories.touched ? categories.error : ''}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea className="form-control" {...content} />
+                    <div className="text-help">
+                        {content.touched ? content.error : ''}
+                    </div>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -32,6 +44,16 @@ class PostsNew extends Component {
 
 function validate(values) {
     const errors = {};
+
+    if (!values.title) {
+        errors.title = 'Enter a username';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter a category';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content';
+    }
 
     return errors;
 }
@@ -41,5 +63,6 @@ function validate(values) {
 
 export default reduxForm({
     form: 'PostsNewForm',
-    fields: ['title', 'categories', 'content']
+    fields: ['title', 'categories', 'content'],
+    validate
 }, null, { createPost })(PostsNew);
